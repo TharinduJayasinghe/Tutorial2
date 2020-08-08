4f8479b36fd3d46bee1518b4706d8f71a38a0f4d
Tharindu Jayasinghe <tharindujayasinghe99@gmail.com>
2020-08-07 23:12:59 -0800

    Initial commit

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView txtVMsg2 = findViewById(R.id.TvMsg2);
+        txtVMsg2.setText(R.string.Msg2);
+        Log.i( "Lifecycle", "OnCreate Called..");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle", "onstart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lifecycle", "onresume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lifecycle", "onpause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Lifecycle", "onstop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle", "ondestroy called");
+    }
+}
